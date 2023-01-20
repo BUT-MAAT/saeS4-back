3bf624db2cacb4ac73f85f65a8b5b8a325b6e7ed
Alexandre <alexandre.bidaux78@gmail.com>
2023-01-20 15:20:48 +0100

    init spring env

---
+++ src/main/java/com/saes4/saes4/SaeS4Application.java
@@ -0,0 +1,13 @@
+package com.saes4.saes4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeS4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeS4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/saes4/saes4/SaeS4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saes4.saes4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeS4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
