447d90328dce4e81160442ef17994cf5e3243251
Alexandre BIDAUX <113607915+StutenEXE@users.noreply.github.com>
2023-02-09 13:40:24 +0100

    Revert "Aliment services"

--- src/main/java/com/saes4/saes4/model/ValeursNutritives.java
+++ src/main/java/com/saes4/saes4/model/ValeursNutritives.java
@@ -14,73 +14,9 @@ import java.io.Serializable;
 @Table(name = "valeurs_nutritives")
 public class ValeursNutritives implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @PrimaryKeyJoinColumn(name = "id_valeurs_nutritives")
     private Long id_valeurs_nutritives;
-
-    private Double energie_ue_kj;
-    private Double energie_ue_kcal;
-    private Double energie_jones_kj;
-    private Double energie_jones_kcal;
-    private Double eau;
-    private Double proteines_Jones;
-    private Double proteines_625;
-    private Double glucides;
-    private Double lipides;
-    private Double sucres;
-    private Double fructose;
-    private Double galactose;
-    private Double glucose;
-    private Double lactose;
-    private Double maltose;
-    private Double saccharose;
-    private Double amidon;
-    private Double fibres_alimentaires;
-    private Double polyols_totaux;
-    private Double cendres;
-    private Double alcool;
-    private Double acides_organiques;
-    private Double ag_satures;
-    private Double ag_monoinsatures;
-    private Double ag_polyinsatures;
-    private Double ag_butriques;
-    private Double ag_caproique;
-    private Double ag_caprylique;
-    private Double ag_caprique;
-    private Double ag_laurique;
-    private Double ag_myristique;
-    private Double ag_palmitique;
-    private Double ag_steraique;
-    private Double ag_oleique;
-    private Double ag_linoleique;
-    private Double ag_alpha_linoleique;
-    private Double ag_arachidonique;
-    private Double ag_epa;
-    private Double ag_dha;
-    private Double cholesterol;
-    private Double sel;
-    private Double calcium;
-    private Double chlorure;
-    private Double cuivre;
-    private Double fer;
-    private Double iode;
-    private Double magnesium;
-    private Double manganese;
-    private Double phosphore;
-    private Double potassium;
-    private Double selenium;
-    private Double sodium;
-    private Double zinc;
-    private Double retinol;
-    private Double beta_carotene;
-    private Double vitamine_D;
-    private Double vitamine_E;
-    private Double vitamine_K1;
-    private Double vitamine_K2;
-    private Double vitamine_C;
-    private Double vitamine_B1;
-    private Double vitamine_B2;
-    private Double vitamine_b3;
-    private Double vitamine_b5;
 }
