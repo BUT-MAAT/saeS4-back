62c4a56f8f70ba7d9e0474e66d945785b562286b
Alexandre <alexandre.bidaux78@gmail.com>
2023-02-17 16:15:32 +0100

    Tests utils

--- src/main/java/com/saes4/saes4/service/UtilsService.java
+++ src/main/java/com/saes4/saes4/service/UtilsService.java
@@ -1,5 +1,6 @@
 package com.saes4.saes4.service;
 
+import com.saes4.saes4.model.UniqueData;
 import com.saes4.saes4.model.UniqueResponse;
 import com.saes4.saes4.repository.SondageRepository;
 import jakarta.transaction.Transactional;
@@ -13,8 +14,8 @@ public class UtilsService {
     @Autowired
     private SondageRepository sondageRepository;
 
-    public UniqueResponse<Boolean> mailIsValide(String mail) {
-        Boolean isValid = !sondageRepository.existsByMail(mail);
+    public UniqueResponse<Boolean> mailIsValide(UniqueData<String> mail) {
+        Boolean isValid = !sondageRepository.existsByMail(mail.getData());
         UniqueResponse<Boolean> response = new UniqueResponse();
         response.setResponse(isValid);
         return response;
