1671365ccb42979901e917a4f1b3c9899423a8f4
Alexandre <alexandre.bidaux78@gmail.com>
2023-02-17 15:16:01 +0100

    GetMapping -> PostMapping

--- src/main/java/com/saes4/saes4/controller/UtilsController.java
+++ src/main/java/com/saes4/saes4/controller/UtilsController.java
@@ -2,10 +2,7 @@ package com.saes4.saes4.controller;
 
 import com.saes4.saes4.service.UtilsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -15,7 +12,7 @@ public class UtilsController {
     @Autowired
     private UtilsService utilsService;
 
-    @GetMapping("/mail_valide")
+    @PostMapping("/mail_valide")
     public boolean mailIsValide(@RequestBody String mail) {
         return utilsService.mailIsValide(mail);
     }
