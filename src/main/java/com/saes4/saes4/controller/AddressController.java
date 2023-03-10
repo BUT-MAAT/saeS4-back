afd8a60426cfcfc2c3119e162b44e8f5de0c3ec7
Mael Chanon <mael.chanon@etu.u-paris.fr>
2023-03-10 13:05:22 +0100

    changed endpoint and added some documentation

--- src/main/java/com/saes4/saes4/controller/AddressController.java
+++ src/main/java/com/saes4/saes4/controller/AddressController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/postal")
+@RequestMapping("/api/address")
 public class AddressController {
     @Autowired
     AddressService addressService;
